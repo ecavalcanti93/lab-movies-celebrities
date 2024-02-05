35a9c06a2785273e6f6f843afddd2da47cec52f0
ecavalcanti683 <ecavalcanti.dg@outlook.com>
2024-02-05 15:53:50 +0100

    Done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
-//  Add your code here
+//!Interation #2: The Celebrity model
+
+const { Schema, model } = require('mongoose');
+
+const celebritySchema = new Schema({
+
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+})
+
+module.exports = model('Celebrity', celebritySchema);
